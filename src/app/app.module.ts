5a7acc5dc5ef8d7768b3b49ca966243740ae5589
Nguyen-Van-Chuong <107835819+Nguyen-Van-Chuong@users.noreply.github.com>
2023-05-11 16:43:16 +0700

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [NavbarComponent],
 })
-export class AppModule { }
+export class AppModule {}
