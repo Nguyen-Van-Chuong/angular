c2ea58e86dd04003f92bc09f750c861fbfff7ea2
Chuong Nguyen <chuongsanji@gmail.com>
2023-05-13 11:10:53 +0700

    edit lab2

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,7 +10,7 @@ export class CardComponent {
     hoten: 'Nguyễn Văn Chương',
     gioitinh: 'Nam',
     ngaysinh: '6/1/2003',
-    anh: 'poly.jpeg',
+    anh: 'https://media.newyorker.com/photos/5e4ef70761934700086187b8/1:1/w_1698,h_1698,c_limit/Younger-PopSmoke.jpg',
     diemTB: 8.9,
   };
 }
