7e85e70d80ca42f284c530536ce7eb66030ba9e7
Chuong Nguyen <chuongsanji@gmail.com>
2023-05-14 23:28:40 +0700

    change again

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,11 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  student = {
-    hoten: 'Nguyễn Văn Chương',
-    gioitinh: 'Nam',
-    ngaysinh: '6/1/2003',
-    anh: 'https://media.newyorker.com/photos/5e4ef70761934700086187b8/1:1/w_1698,h_1698,c_limit/Younger-PopSmoke.jpg',
-    diemTB: 8.9,
-  };
+  students = [
+    {
+      hoten: 'Nguyễn Văn Chương',
+      gioitinh: 'Nam',
+      ngaysinh: '6/1/2003',
+      anh: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/07/01/23/web-aliens-2.jpg',
+      diemTB: 8.9,
+    },
+    {
+      hoten: 'Nguyễn Văn Chương2',
+      gioitinh: 'Nam',
+      ngaysinh: '6/1/2003',
+      anh: 'https://media.newyorker.com/photos/5e4ef70761934700086187b8/1:1/w_1698,h_1698,c_limit/Younger-PopSmoke.jpg',
+      diemTB: 8.9,
+    },
+    {
+      hoten: 'Nguyễn Văn Chương',
+      gioitinh: 'Nam',
+      ngaysinh: '6/1/2003',
+      anh: 'https://media.newyorker.com/photos/5e4ef70761934700086187b8/1:1/w_1698,h_1698,c_limit/Younger-PopSmoke.jpg',
+      diemTB: 8.9,
+    },
+    {
+      hoten: 'Nguyễn Văn Chương',
+      gioitinh: 'Nam',
+      ngaysinh: '6/1/2003',
+      anh: 'https://media.newyorker.com/photos/5e4ef70761934700086187b8/1:1/w_1698,h_1698,c_limit/Younger-PopSmoke.jpg',
+      diemTB: 8.9,
+    },
+    {
+      hoten: 'Nguyễn Văn Chương',
+      gioitinh: 'Nam',
+      ngaysinh: '6/1/2003',
+      anh: 'https://media.newyorker.com/photos/5e4ef70761934700086187b8/1:1/w_1698,h_1698,c_limit/Younger-PopSmoke.jpg',
+      diemTB: 8.9,
+    },
+    {
+      hoten: 'Nguyễn Văn Chương',
+      gioitinh: 'Nam',
+      ngaysinh: '6/1/2003',
+      anh: 'https://media.newyorker.com/photos/5e4ef70761934700086187b8/1:1/w_1698,h_1698,c_limit/Younger-PopSmoke.jpg',
+      diemTB: 8.9,
+    },
+  ];
 }
