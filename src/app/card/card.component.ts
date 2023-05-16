9b924e1b250017574b4cdc42ae027f455ad141ee
Chuong Nguyen <chuongsanji@gmail.com>
2023-05-16 19:20:09 +0700

    add

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { students } from '../entities/students';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  students = [
+  students: students[] = [
     {
       hoten: 'Nguyễn Văn Chương',
       gioitinh: 'Nam',
---
+++ src/app/entities/students.ts
@@ -0,0 +1,7 @@
+export interface students {
+  hoten: string;
+  gioitinh: string;
+  ngaysinh: string;
+  anh: string;
+  diemTB: number;
+}
