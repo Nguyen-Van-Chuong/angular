99b4a4d917566fa369ebdbbeae58bc1e01d9043e
Chuong Nguyen <chuongsanji@gmail.com>
2023-05-27 19:12:30 +0700

    update

--- src/app/productdeltail/productdeltail.component.ts
+++ src/app/productdeltail/productdeltail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IProduct } from '../entities/product';
 import { ActivatedRoute } from '@angular/router';
 
